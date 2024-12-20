51c54f98c73861dc9cfb6d1b924e1ae2b5ee0752
Aaron Wright <aarnwri@gmail.com>
2024-12-20 11:24:38 -0800

    Refactor bracket list command to start simplifying option helpers

---
+++ lib/bin_helpers/bracket_cmd/list.rb
@@ -0,0 +1,17 @@
+class BracketCmd; end
+class BracketCmd::List < BracketCmd
+  def initialize
+    @options = {}
+    @parser  = OptionParser.new do |opts|
+      opts.banner = "Usage: bracket list [options]"
+    end
+
+    @parser.parse!(into: @options)
+  end
+
+  def run
+    puts "Brackets:"
+    puts ""
+    puts Bracket.list_all
+  end
+end
